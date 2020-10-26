09fe6ce51042612be7d6213faab7dd01425859b1
Goel Shubham (RBEI/EAA2) <hgs9kor@bosch.com>
2020-10-26 13:58:36 +0530

    updated coin components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
+import CoinContainer from './CoinContainer'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Test
+      <CoinContainer />
     </div>
   );
 }
